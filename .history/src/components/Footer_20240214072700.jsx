dc78588a7fd80b9d58d6f2cb2831c1ee070b8124
9a_orn <Lum.Gayoung@gmail.com>
2024-02-14 07:43:02 +0900

    home, header, footer 완성

---
+++ .history/src/components/Footer_20240214072700.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className='flex left-1/2 transform -translate-x-1/2 w-screen bg-blue h-48 text-center text-white justify-center items-center '>
+      <div>
+        <p>dkssudgktpdyd</p>
+      </div>
+    </div>
+  );
+}
---
+++ .history/src/components/Introduce_20240214051642.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import useScrollFadeIn from '../hooks/useScrollFadeIn';
+import useScrollClipPath from '../hooks/useScrollClipPath';
+
+const Introduce = () => {
+  const animatedItem1 = useScrollFadeIn('right', 1, 0);
+  const animatedImage = useScrollClipPath('left', 1, 0);
+
+  return (
+    <section className='relative left-1/2 transform -translate-x-1/2 w-screen ml-auto '>
+      <section className=' justify-between items-center py-10 text-center '>
+        <h2 className='text-brand pt-4 text-xl font-bold'>Our Services</h2>
+        <div className='flex justify-center items-center py-10 w-max_width mx-auto'>
+          <div className='w-1/4 p-5 m-10 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] border-spacing-5'>
+            <h3 className='text-2xl text-text font-bold py-3'>
+              Passionate Dog Owners
+            </h3>
+            <hr />
+            <p className='text-md text-text py-3'>
+              Connect with dog owners who share your love for these wonderful
+              companions.
+            </p>
+          </div>
+          <div className='w-1/4 p-5 m-10 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]'>
+            <h3>Passionate Dog Owners</h3>
+            <p>
+              Connect with dog owners who share your love for these wonderful
+              companions.
+            </p>
+          </div>
+          <div className='w-1/4 p-5 m-10 shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]'>
+            <h3>Passionate Dog Owners</h3>
+            <p>
+              Connect with dog owners who share your love for these wonderful
+              companions.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className='py-10 flex items-center justify-between  bg-lightYello1'>
+        <div className=' flex justify-between items-center py-10 w-max_width mx-auto'>
+          <div className='w-introduceImg ml-10' {...animatedImage}>
+            <img
+              className='w-full'
+              src='images/introduceImg2.png'
+              alt='introduceImg2'
+            />
+          </div>
+          <div className=' text-center mr-16'>
+            <h1 className='text-5xl font-bold'>간편하게</h1>
+            <br />
+            <p className='text-2xl'>We connect people with their dog</p>
+          </div>
+        </div>
+      </section>
+
+      <section className='py-10 flex items-center  justify-between '>
+        <div className=' flex justify-between items-center py-10 w-max_width mx-auto'>
+          <div className='text-center ml-16' {...animatedItem1}>
+            <h1 className='text-5xl font-bold'>실시간 이동 경로 확인</h1>
+            <br />
+            <p className='text-2xl'>We connect people with their dog</p>
+          </div>
+          <div className='w-introduceImg rounded-2 overflow-hidden mr-10'>
+            <img
+              className='w-full'
+              src='images/introduceImg3.jpg'
+              alt='introduceImg3'
+            />
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default Introduce;
