ad98559d80ea33086ebec7fd1786dd1b5dc5acf8
9a_orn <Lum.Gayoung@gmail.com>
2024-03-06 03:23:22 +0900

    로그인 연결까지

--- .history/src/components/member/Login_20240306022302.jsx
+++ .history/src/components/member/Login_20240306022302.jsx
@@ -5,7 +5,7 @@ export default function Login() {
   const NaverLogin = () => {
     const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID;
     const REDIRECT_URI = 'http://localhost:3000/callback'; // Callback URL
-    const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?scope=name,email,age,gender,birthday,birthyear,mobile&response_type=code&redirect_uri=${REDIRECT_URI}&client_id=${NAVER_CLIENT_ID}`;
+    const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?scope=name,email,age,gender,birthday,birthyear,mobile&response_type=code&redirect_uri=http://localhost:8080/auth/naver/callback&client_id=YzcMnUhYjgc_1udqR96m`;
 
     window.location.href = NAVER_AUTH_URL;
 
