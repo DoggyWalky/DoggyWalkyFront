9441d554ea412563e83f8e95af669b183605415d
Jiyoung <jssq2468@naver.com>
2024-05-26 16:59:49 +0900

    ㅋㅓ밋

--- src/components/member/Login.jsx
+++ src/components/member/Login.jsx
@@ -8,7 +8,7 @@ export default function Login() {
     const callbackUrl = 'http://localhost:3000/NaverLogin'; // Callback URL
     const stateString = false;
     const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?client_id=${NAVER_CLIENT_ID}&response_type=code&redirect_uri=${callbackUrl}&state=${stateString}`;
-    // const NAVER_AUTH_URL = `/auth/NAVER`;
+    // const NAVER_AUTH_URL = `/auth/NAVER `;
 
     window.location.href = NAVER_AUTH_URL;
   };
